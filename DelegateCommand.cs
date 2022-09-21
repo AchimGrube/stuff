fba68a05ad5b589dd9720cc950f61041f2fd7e59
Achim Grube <12234126+AchimGrube@users.noreply.github.com>
2022-09-22 00:36:20 +0200

    Create DelegateCommand.cs

---
+++ DelegateCommand.cs
@@ -0,0 +1,15 @@
+public class DelegateCommand : ICommand
+{
+  private readonly Action<object> _execute;
+  private readonly Predicate<object> _canExecute;
+  
+  public evene EventHandler CanExecuteChanged;
+  
+  public DelegateCommand(Predicate<object> canExecute, Action<object> execute) => (canExecute, execute) = (_canExecute, _execute);
+  
+  public DelegateCommand(Action<object> execute) : this (null, execute) { }
+  
+  public bool CanExecute(object parameter) => _canExecute?.Invoce(parameter) ?? true;
+  public bool Execute(object parameter) => _execute?.Invoke(parameter);
+  
+  public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
